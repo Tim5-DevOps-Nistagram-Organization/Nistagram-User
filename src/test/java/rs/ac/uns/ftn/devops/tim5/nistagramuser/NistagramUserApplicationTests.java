481593618032db65fc838dd8cff9829a7a19d593
MicaTravica <40805992+MicaTravica@users.noreply.github.com>
2021-06-06 16:03:29 +0200

    init test

--- src/test/java/rs/ac/uns/ftn/devops/tim5/nistagramuser/NistagramUserApplicationTests.java
+++ src/test/java/rs/ac/uns/ftn/devops/tim5/nistagramuser/NistagramUserApplicationTests.java
@@ -3,12 +3,14 @@ package rs.ac.uns.ftn.devops.tim5.nistagramuser;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.Assert.assertEquals;
+
 @SpringBootTest
 class NistagramUserApplicationTests {
 
 	@Test
 	void contextLoads() {
-		assert 1 == 1;
+		assertEquals(1,1);
 	}
 
 }
