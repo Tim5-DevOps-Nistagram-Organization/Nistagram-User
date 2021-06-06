e38ca0420e482292dd08a8d0c23a6a2ea6fb55bf
MicaTravica <40805992+MicaTravica@users.noreply.github.com>
2021-06-06 15:43:42 +0200

    update pom.xml

--- create-test-env-run-test.sh
+++ create-test-env-run-test.sh
@@ -8,7 +8,7 @@ base=${4:-main}
 source ./config/.env.test
 
 if [ "$pullKey" = "0" ]; then
-  SONAR_TOKEN=${token} BRANCH_NAME=${branch} COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose -f ./docker-compose.test.yml  --env-file ./config/.env.test up --build -d 
+  SONAR_TOKEN=${token} BRANCH_NAME=${branch} COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose -f ./docker-compose.test.yml  --env-file ./config/.env.test up --build 
 else
   SONAR_TOKEN=${token} PULL_KEY=${pullKey} PULL_REQUEST_BRANCH=${branch} BASE=${base} COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose -f ./docker-compose.test.yml  --env-file ./config/.env.test up --build -d
 fi
