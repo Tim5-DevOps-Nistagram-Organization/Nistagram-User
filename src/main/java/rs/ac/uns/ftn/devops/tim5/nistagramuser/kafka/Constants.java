08a661583f7697317f59e7109aa51ec42d779a77
MicaTravica <40805992+MicaTravica@users.noreply.github.com>
2021-06-25 19:00:55 +0200

    Set up auth for user microservice #24 (#25)
    * Set up auth for user microservice #24
    
    * update, fixed bug

---
+++ src/main/java/rs/ac/uns/ftn/devops/tim5/nistagramuser/kafka/Constants.java
@@ -0,0 +1,8 @@
+package rs.ac.uns.ftn.devops.tim5.nistagramuser.kafka;
+
+public class Constants {
+
+    public static final String GROUP = "my_group_id";
+    public static final String AUTH_TOPIC = "auth_topic";
+    public static final String AUTH_USER_TOPIC = "auth_user_topic";
+}
