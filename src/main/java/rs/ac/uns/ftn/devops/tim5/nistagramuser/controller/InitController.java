53ce5eca340a113023b35fb85df2af24a3a2e106
MicaTravica <40805992+MicaTravica@users.noreply.github.com>
2021-06-25 12:34:44 +0200

    Set up auth for user microservice #24

--- src/main/java/rs/ac/uns/ftn/devops/tim5/nistagramuser/controller/InitController.java
+++ src/main/java/rs/ac/uns/ftn/devops/tim5/nistagramuser/controller/InitController.java
@@ -1,12 +1,32 @@
 package rs.ac.uns.ftn.devops.tim5.nistagramuser.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/init")
 public class InitController {
+
+
     @GetMapping
-    public String init() {
-        return "Hello, Nistagram User";
+    public ResponseEntity<String> getAllSales() {
+        return new ResponseEntity<>("Hello, Nistagram User", HttpStatus.OK);
     }
+
+    @GetMapping(value = "/admin")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<String> admin() {
+        return new ResponseEntity<>("Hello, Nistagram User", HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/regular")
+    @PreAuthorize("hasRole('ROLE_REGULAR')")
+    public ResponseEntity<String> regular() {
+        return new ResponseEntity<>("Hello, Nistagram User", HttpStatus.OK);
+    }
+
 }
