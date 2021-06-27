4aad704f1a950100b0cf0f3154ebe7256d86b544
MicaTravica <40805992+MicaTravica@users.noreply.github.com>
2021-06-27 17:35:41 +0200

    Edit user profile #3

--- src/main/java/rs/ac/uns/ftn/devops/tim5/nistagramuser/service/UserService.java
+++ src/main/java/rs/ac/uns/ftn/devops/tim5/nistagramuser/service/UserService.java
@@ -1,7 +1,14 @@
 package rs.ac.uns.ftn.devops.tim5.nistagramuser.service;
 
+import rs.ac.uns.ftn.devops.tim5.nistagramuser.exception.ResourceNotFoundException;
+import rs.ac.uns.ftn.devops.tim5.nistagramuser.model.User;
+
 public interface UserService {
+    User findByUsername(String username) throws ResourceNotFoundException;
+
     void create(String username, String email);
 
-    void delete(String username);
+    void delete(String username) throws ResourceNotFoundException;
+
+    void update(User user, String username) throws ResourceNotFoundException;
 }
